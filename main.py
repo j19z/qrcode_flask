fc526b5eadb7b94309e2a06ba38a5311148da09a
J19Z <jgmusso@gmail.com>
2023-04-11 13:52:24 -0400

    Changed the render function and added coe on HTML
    Fixed an error on the main.py index function to call the correct htlm
    file on the render function. Added a tag on the display html to show
    the code that is printing in the QR.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('qrcode_create.html')
 
 
 @app.route('/qrcode', methods=['POST'])
@@ -23,24 +23,10 @@ def generate_qrcode():
     #response = make_response(img_io.getvalue())
     img_base64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
     #img_base64.headers['Content-Type'] = 'image/png'
-    return render_template('qrcode_display.html', img_base64=img_base64)
+    return render_template('qrcode_display.html', img_base64=img_base64, code=code)
     #return response
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
 
-
-
-# @app.route('/qrcode/<code>', methods=['POST'])
-# def generate_qrcode(code):
-#     qr = qrcode.QRCode(version=1, box_size=10, border=5)
-#     qr.add_data(code)
-#     qr.make(fit=True)
-#     img = qr.make_image(fill_color="black", back_color="white")
-#     img_io = BytesIO()
-#     img.save(img_io, 'PNG')
-#     img_io.seek(0)
-#     response = make_response(img_io.getvalue())
-#     response.headers['Content-Type'] = 'image/png'
-#     return response
